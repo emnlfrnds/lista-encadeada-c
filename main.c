18a1f58172defc138f591ed34bdabc33919ce968
Enzo Capitani <enzoroadtoglory17@aluno.senai.br>
2026-09-08 17:00:47 -0300

    feat: Adicionada a função de push_back e push front do simples

--- main.c
+++ main.c
@@ -35,6 +35,38 @@ NoDuplo* criar_noDuplo(int valor){
     return novo_no;
 }
 
+void push_front_simples(No** cabeca, int valor){
+    No* novo = criar_no(valor);
+
+    //Verifica se o nó é null
+    if (!novo) return;
+
+    //Avisa que o novo cabeça da lista eh ele
+    novo->proximo = *cabeca;
+    *cabeca = novo;
+}
+
+void push_back_simples(No** cabeca, int valor){
+    No* novo = criar_no(valor);
+    //Verifica se o nó é null
+    if (!novo) return;
+
+    // Verifica se nao tem nenhum nó na lista
+    if(*cabeca == NULL){
+        *cabeca = novo;
+        return;
+    }
+
+    // Procura o ultimo nó da lista
+    No* aux = *cabeca;
+    while(aux->proximo != NULL){
+        aux = aux->proximo;
+    }
+
+    // Adiciona o novo nó no final da lista que foi encontrado
+    aux->proximo = novo;
+}
+
 int main() {
     No* simples = NULL;
     NoDuplo* duplo = NULL;
