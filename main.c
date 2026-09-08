40dbae94b9434d1de496bc6b94041e37f59198d6
Enzo Capitani <enzoroadtoglory17@aluno.senai.br>
2026-09-08 16:38:50 -0300

    feat: Adição das structs dos nós para a lista encadeada

--- main.c
+++ main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 
-int main() {
+typedef struct No {
+    int valor;
+    struct No* proximo;
+} No;
 
-    printf("ola mundo");
+typedef struct NoDuplo {
+    int valor;
+    struct No* proximo;
+    struct No* anterior;
+} NoDuplo;
+
+No* criar_no(int valor){
+    No* novo_no = (No*)malloc(sizeof(No));
+
+    if(novo_no){
+        novo_no->valor = valor;
+        novo_no->proximo = NULL;
+    }
+
+    return novo_no;
+}
+
+NoDuplo* criar_noDuplo(int valor){
+    NoDuplo* novo_no = (NoDuplo*)malloc(sizeof(NoDuplo));
+
+    if(novo_no){
+        novo_no->valor = valor;
+        novo_no->proximo = NULL;
+        novo_no->anterior = NULL;
+    }
+
+    return novo_no;
+}
+
+int main() {
+    No* simples = NULL;
+    NoDuplo* duplo = NULL;
 
     return 0;
 }
