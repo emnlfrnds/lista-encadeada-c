b9c364ce4ce123a9d390b177d8bddc60e8e35f5b
Henry Kanemoto <kanemotohenry@gmial.gmail>
2026-09-08 17:26:35 -0300

    feat : Adicona função de lista e apagar o primeiro

--- main.c
+++ main.c
@@ -8,8 +8,8 @@ typedef struct No {
 
 typedef struct NoDuplo {
     int valor;
-    struct No* proximo;
-    struct No* anterior;
+    struct NoDuplo* proximo;
+    struct NoDuplo* anterior;
 } NoDuplo;
 
 No* criar_no(int valor){
@@ -35,9 +35,44 @@ NoDuplo* criar_noDuplo(int valor){
     return novo_no;
 }
 
+void listar_NoSimples(No* inicio){
+    No* atual = inicio;
+    while(atual != NULL) {
+        printf("%d ", atual->valor);
+        atual = atual->proximo;
+    }
+    printf("\n");
+}
+void listar_NoDuplo(NoDuplo* inicio){
+    NoDuplo* atual = inicio;
+    while(atual != NULL) {
+        printf("%d ", atual->valor);
+        atual = atual->proximo;
+    }
+    printf("\n");
+}
+
+void apagar_Primeiro_NoSimples (No** cabeca) {
+    No* tmp = *cabeca;
+    *cabeca = (*cabeca)->proximo;
+    free(tmp);
+}
+void apagar_Primeiro_NoDuplo (NoDuplo** cabeca) {
+    NoDuplo* tmp = *cabeca;
+    *cabeca = (*cabeca)->proximo;
+    free(tmp);
+}
+
 int main() {
     No* simples = NULL;
     NoDuplo* duplo = NULL;
+
+    duplo = criar_noDuplo(1);
+    duplo->proximo = criar_noDuplo(4);
+    duplo->proximo->anterior = duplo;
+
+    apagar_Primeiro_NoDuplo(&duplo);
+    listar_NoDuplo(duplo);
 
     return 0;
 }
